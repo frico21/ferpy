86266eef4b3ddf6d37d8bdfcd4427afaa48d79bc
frico21 <Fernando.ricovillas@gmail.com>
2018-11-30 13:52:38 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ferpy',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Astrophysics utils',
+    long_description=open('README.md').read(),
+    install_requires=['numpy', 'astropy', 'scipy'],
+    url='https://github.com/frico21/ferpy',
+    author='Fernando Rico',
+    author_email='fer91green@gmail.com'
+)
