33d0162bd8ded70600028ebd5a022ea12966d53f
frico21 <Fernando.ricovillas@gmail.com>
2018-12-03 13:04:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ferpy',
-    #version='0.0.1',
+    version='0.0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Astrophysics utils',
     long_description=open('README.md').read(),
-    install_requires=['numpy', 'astropy', 'scipy', 'math'],
+    install_requires=['numpy', 'astropy', 'scipy'],
     url='https://github.com/frico21/ferpy',
     author='Fernando Rico',
     author_email='fer91green@gmail.com'
