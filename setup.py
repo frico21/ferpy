f283b32f73d7d250b7e3477697f366701a5884d7
frico21 <Fernando.ricovillas@gmail.com>
2018-12-03 13:01:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ferpy',
-    version='0.0.1',
+    #version='0.0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Astrophysics utils',
